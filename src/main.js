f52010402bb317737e8c515ac6ed7b78f6b6fa2f
wangxiaobai2019 <1510106069@qq.com>
2022-04-20 15:49:08 +0800

    feat: 环境变量配置

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from "./router";
+import axios from 'axios';
+import config from './config'
+
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 const app = createApp(App)
+axios.get(`${config.mockApi}/login`).then(res => console.log(res))
 app.use(Router)
 app.use(ElementPlus)
 app.mount('#app')
