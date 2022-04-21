18e5f461275aa30b23bed0756f847f2de7a20cc0
wangxiaobai2019 <1510106069@qq.com>
2022-04-21 15:22:01 +0800

    feat: localstorage二次封装

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from "./router";
 import request from './utils/request';
-
+import storage from "./utils/storage";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 const app = createApp(App)
 app.config.globalProperties.$request = request
+app.config.globalProperties.$storage = storage
 app.use(Router)
 app.use(ElementPlus)
 app.mount('#app')
